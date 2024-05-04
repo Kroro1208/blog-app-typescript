4a5415d945fbbd06283ee2f6fdfa28229d2dc213
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-04 12:54:54 +0900

    共通で使用する認証、テーマカラーの切り替えに使用するプロバイダーを実装した

--- app/context/index.tsx
+++ app/context/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react"
+import { createContext, Dispatch, SetStateAction, useState } from "react"
 
 type ContextType = {
     loading: boolean;
@@ -14,11 +14,11 @@ const initialState = {
 
 const GlobalContext = createContext<ContextType>(initialState);
 
-export default function GlobalState({ children }: { children: ReactNode }) {
+export default function GlobalState({ children }: { children: React.ReactNode }) {
     const [loading, setLoading] = useState(false);
     return (
         <GlobalContext.Provider value={{ loading, setLoading }}>
-
+            {children}
         </GlobalContext.Provider >
     );
 }
