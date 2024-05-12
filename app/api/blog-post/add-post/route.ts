6c01fa37184839f164b4c0d04b82ec2be692bb99
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-12 21:50:48 +0900

    prismaのcreate()の成功を確認

--- app/api/blog-post/add-post/route.ts
+++ app/api/blog-post/add-post/route.ts
@@ -1,10 +1,25 @@
+import prisma from "@/app/database";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const extractPostData = await request.json();
-    const newlyCreatePost = await 
+    const newlyCreatePost = await prisma.post.create({
+      data: extractPostData,
+    });
     console.log(extractPostData);
+
+    if (newlyCreatePost) {
+      return NextResponse.json({
+        success: true,
+        message: "新しいブログが投稿されました",
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "ブログの投稿に失敗しました",
+      });
+    }
   } catch (error) {
     console.log(error);
     return NextResponse.json({
