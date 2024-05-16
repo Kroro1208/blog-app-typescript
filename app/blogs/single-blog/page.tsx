e4e99ae09cff16b0a76ee4023ed05713170101ad
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-16 22:00:36 +0900

    外部画像のホスト名を明示的に設定した

--- app/blogs/single-blog/page.tsx
+++ app/blogs/single-blog/page.tsx
@@ -1,7 +1,18 @@
-const SingleBlog = () => {
+import { Blog } from "@/app/utils/types"
+import Image from "next/image"
+import Link from "next/link"
+
+const SingleBlog = ({ blogItem }: { blogItem: Blog }) => {
+    const { image, category } = blogItem;
     return (
-        <div>
-            SingleBlog
+        <div className="relative overflow-hidden rounded-md bg-white shadow-one dark:bg-dark">
+            <Link href={"/"}>
+                <span className="absolute top-6 z-20 inline-flex items-center justify-center
+                rounded-full bg-primary py-2 px-4 font-semibold capitalize text-white">
+                    {category}
+                </span>
+                <Image src={image} alt="BlogPost" fill />
+            </Link>
         </div>
     )
 }
--- app/components/blogs/blog-list/page.tsx
+++ app/components/blogs/blog-list/page.tsx
@@ -1,17 +1,23 @@
 "use client"
-
 import SingleBlog from "@/app/blogs/single-blog/page"
 import { Blog } from "@/app/utils/types"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function BlogLists({ lists }: { lists: Blog[] }) {
+    const router = useRouter();
+    useEffect(() => {
+        router.refresh();
+    }, []);
+
     return (
         <section className="pt-[120px] pb-[120px]">
             <div className="container">
                 <div className="-mx-4 grid grid-cols-3 gap-2">
                     {
-                        lists && lists.length ? lists.map((listItem: Blog) => (
+                        (lists && lists.length) ? lists.map((listItem: Blog) => (
                             <div className="px-4" key={listItem.id}>
-                                <SingleBlog />
+                                <SingleBlog blogItem={listItem} />
                             </div>))
                             : null
                     }
