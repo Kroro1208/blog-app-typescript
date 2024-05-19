839cd5cdff4be319a4f55e5bc98e6877914e77d7
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-19 13:03:41 +0900

    コメント入力欄実装した

--- app/components/Button/button.tsx
+++ app/components/Button/button.tsx
@@ -1,7 +1,7 @@
 export default function Button({ text, onClick }: { text: string, onClick: () => void; }) {
     return (
         <button
-            className="bg-primary py-3 px-4 rounded-full text-base font-medium
+            className="bg-primary py-3 px-6 rounded-2xl font-medium
              text-white transition duration-300 ease-out hover:bg-opacity-80 hover:shadow-signUp"
             onClick={onClick} >
             {text}
--- app/components/blogs/blog-details/index.tsx
+++ app/components/blogs/blog-details/index.tsx
@@ -1,14 +1,75 @@
 "use client"
 
 import { Blog } from "@/app/utils/types"
+import Image from "next/image";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 
 const BlogDetailsHome = ({ blogData }: { blogData: Blog }) => {
     console.log(blogData);
+    if (!blogData) return null;
 
     return (
-        <div>
-            ブログの詳細
-        </div>
+        <section className="pt-[150px] pb-[120px]">
+            <div className="container">
+                <div className="-mx-4 flex flex-col gap-4 items-center justify-center">
+                    <div className="w-full px-4 lg:w-8/12">
+                        <h2 className="mb-8 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl">
+                            {blogData?.title}
+                        </h2>
+                        <div className="mb-10 flex flex-wrap items-center justify-between border-b
+                         border-body-color border-opacity-50 dark:border-white dark:border-opacity-10">
+                            <div className="flex flex-wrap items-center">
+                                <div className="mr-10 mb-5 flex items-center">
+                                    <div className="mr4">
+                                        <div className="relative h-10 w-10 overflow-hidden rounded-full">
+                                            <Image src={blogData?.userImage} alt="User" fill />
+                                        </div>
+                                    </div>
+                                    <div className="w-full flex">
+                                        <h4 className="mb-1 ml-2 text-base font-medium text-body-color">
+                                            by
+                                        </h4>
+                                        <span className="pl-2">{blogData?.userId.split('_')[0]}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mb-5">
+                                <Link
+                                    className="inline-flex items-center justify-center rounded-full bg-primary py-2 px-4 text-sm font-semibold text-white"
+                                    href={`/category/${blogData?.category}`}>
+                                    {blogData?.category}
+                                </Link>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="mb-10 w-full overflow-hidden rounded-xl">
+                                <div className="relative aspect-[97/60] w-full sm:aspect-[97/44]">
+                                    <Image
+                                        className="object-cover object-center"
+                                        src={blogData?.image} alt="Blog" fill />
+                                </div>
+                            </div>
+                            <p className="mb-8 leading-relaxed text-base font-medium text-body-color sm:text-lg lg:text-base xl:text-lg">
+                                {blogData?.description}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-full lg:w-8/12 flex gap-4">
+                        <input
+                            id="comment" type="text" name="comment" placeholder="コメントを入力" autoFocus autoComplete="false"
+                            className="w-full rounded-md border border-body-color py-3 px-6 text-base text-body-color
+                     placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none
+                      dark:bg-[#242B51] dark:shadow-signUp"
+                        />
+                        <button className="border border-body-color hover:shadow-signUp hover:bg-body-color rounded-lg py-3 px-4">
+                            <FontAwesomeIcon icon={faPaperPlane} size="lg" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
